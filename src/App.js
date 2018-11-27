2d06320f60f96ec7a11f865ab3ff0171139931a5
Christine Samarchi <csamarchi@gmail.com>
2018-11-27 14:54:17 -0600

    created LandingPage, Celebrity, Movies, Shows, and Music components and made basic pie chart with buttons functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {PieChart} from 'react-easy-chart';
 import {BarChart} from 'react-easy-chart';
-
+import LandingPage from './LandingPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> hi </h1>
+        <LandingPage />
       </div>
     );
   }
---
+++ src/LandingPage/index.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {PieChart} from 'react-easy-chart';
+
+class LandingPage extends Component {
+    constructor() {
+      super();
+      this.state = {
+        valueA: 0,
+        valueB: 0
+      }
+    }
+    handleClickA = (e, prop) => {
+      e.preventDefault();
+      this.setState((prevState) => ({
+        valueA: prevState.valueA + 1
+      }))
+    }
+
+    handleClickB = (e, prop) => {
+      e.preventDefault();
+      this.setState((prevState) => ({
+        valueB: prevState.valueB + 1
+      }))
+    }
+
+
+    render() {
+      return(
+        <div>
+        <PieChart
+        labels data = {[
+          { key: 'A', value: this.state.valueA },
+          { key: 'B', value: this.state.valueB },
+        ]}
+        styles= {{
+          '.chart_text': {
+            fontSize: '1em',
+            fill: '#fff'
+          }
+        }}
+        />
+        <button onClick={this.handleClickA} > Jimmy Fallon </button>
+        <button onClick={this.handleClickB}> Jimmy Kimmel </button>
+        </div>
+      )
+    }
+}
+
+export default LandingPage;
