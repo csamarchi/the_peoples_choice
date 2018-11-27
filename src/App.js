751b952dc6e47adb703a223db6ed8e6117d2bc89
Christine Samarchi <csamarchi@gmail.com>
2018-11-27 13:22:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {PieChart} from 'react-easy-chart';
+import {BarChart} from 'react-easy-chart';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> hi </h1>
       </div>
     );
   }
