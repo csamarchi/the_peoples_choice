5dc2fdc7aa6f449f58f62dd9f3b44ffd99c38349
Christine Samarchi <csamarchi@gmail.com>
2018-12-01 21:01:16 -0600

    gave pie 4 choices and bar 2 choices

--- src/BarChart/index.js
+++ src/BarChart/index.js
@@ -8,7 +8,6 @@ class Bar extends Component {
     this.state = {
       valueA: 0,
       valueB: 0,
-      valueC: 0,
     }
   }
 
@@ -25,12 +24,7 @@ class Bar extends Component {
       valueB: prevState.valueB + 1
     }))
   }
-  handleClickC = (e, prop) => {
-    e.preventDefault();
-    this.setState((prevState) => ({
-      valueC: prevState.valueC + 1
-    }))
-  }
+
 
 
   render() {
@@ -41,18 +35,14 @@ class Bar extends Component {
             <BarChart ylabel='Quantity'
                               width={600}
                               height={400}
-                              
                               margin={{top: 20, right: 20, bottom: 30, left: 40}}
                               data={[
               {text: this.props.name1, value: this.state.valueA},
               {text: this.props.name2, value: this.state.valueB},
-              {text: this.props.name3, value: this.state.valueC}
             ]}/>
 
                   <button onClick={this.handleClickA} > {this.props.name1}</button>
                   <button onClick={this.handleClickB}> {this.props.name2} </button>
-                  <button onClick={this.handleClickC}> {this.props.name3} </button>
-
             </div>
           );
         }
--- src/PieChart/index.js
+++ src/PieChart/index.js
@@ -8,6 +8,8 @@ class Pie extends Component {
     this.state = {
       valueA: 0,
       valueB: 0,
+      valueC: 0,
+      valueD: 0,
     }
   }
 
@@ -24,7 +26,18 @@ class Pie extends Component {
       valueB: prevState.valueB + 1
     }))
   }
-
+  handleClickC = (e, prop) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      valueC: prevState.valueC + 1
+    }))
+  }
+  handleClickD = (e, prop) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      valueD: prevState.valueD + 1
+    }))
+  }
 
   render() {
 
@@ -35,12 +48,16 @@ class Pie extends Component {
               data={[
                 { title: 'One', value: this.state.valueA, color: '#E38627' },
                 { title: 'Two', value: this.state.valueB, color: '#C13C37' },
+                { title: 'Three', value: this.state.valueC, color: '#C13C37' },
+                { title: 'Four', value: this.state.valueD, color: '#C13C37' },
+
               ]}
               style={{height: '300px'}}
             />;
                   <button onClick={this.handleClickA}>{this.props.name1}</button>
                   <button onClick={this.handleClickB}>{this.props.name2}</button>
-
+                  <button onClick={this.handleClickC}>{this.props.name3}</button>
+                  <button onClick={this.handleClickD}>{this.props.name4}</button>
             </div>
           );
         }
